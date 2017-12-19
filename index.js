b6147108269ecbbfc5d72213079a259826685ebf
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-12-19 10:15:52 -0800

    Patching for Heroku

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+
+express()
+  .use(express.static(`${__dirname}/build`))
+  .get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
+  .listen(process.env.PORT, () => {
+    console.log('server up', process.env.PORT);
+});
